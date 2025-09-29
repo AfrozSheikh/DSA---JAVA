f636afbfb7ada81e035e840ac2005e70d6071e89
AfrozSheikh <sa8329925@gmail.com>
2025-09-29 16:20:02 +0530

    heap started

---
+++ Heap/Maxheap.java
@@ -0,0 +1,25 @@
+package Heap;
+
+import java.util.ArrayList;
+
+/**
+ *  Heap
+ */
+public class  Maxheap {
+   List<Integer> heap ;   
+   public Maxheap (){
+      heap = new ArrayList<>() ; 
+   }
+   int peek(){
+      return heap.get(0) ; 
+   }
+   public void heapify(int index){
+      int size = 
+   }
+
+    
+}
+
+public class H {
+    
+}
